153562608b59d604f205187ba49e37461e07ed69
Percyana552 <fernadaguilhermekulyumba@gmail.com>
2023-10-31 10:26:24 +0400

    Create testdict.py

---
+++ Chapter 1- Getting Started with Python/Exercises/testdict.py
@@ -0,0 +1,7 @@
+
+
+# i want to check if bwe want to return one value , so it is a  dictionary
+dictionary = { ' Name':'Petras' ,
+              'Roll no': '250',
+              'Father name ':'Guilherme'}
+print(dictionary.get('Teacher'))
