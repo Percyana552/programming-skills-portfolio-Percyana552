12dc1f7491fe6b34f35ec9ab03df24498549905e
Percyana552 <146834470+Percyana552@users.noreply.github.com>
2023-11-11 13:35:32 +0400

    Add files via upload

---
+++ Chapter 6- Loops/class practice.py
@@ -0,0 +1,16 @@
+# class practice -elif structure
+rate=float(input("Enteryour rate: "))
+if rate>=100:
+    print("your mark is A ")
+elif rate >=50:
+    print("your mark is E")
+elif rate>=80:
+    print("your mark is B")
+else:
+    print("your grade  is C")
+
+
+
+
+
+
